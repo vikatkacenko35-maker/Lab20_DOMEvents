3cdbc8de60ac45dee3229e1dd12f8a72b8aff2f9
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-03-20 15:15:59 +0300

    Step 4: modifying DOM content and styles

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const title = document.getElementById("title");
 const text = document.querySelector(".text");
 console.log(title);
 console.log(text);
+title.textContent = "Dom change";
+title.style.color = "blue";
+title.style.backgroundColor = "#f0f0f0";
+title.style.padding = "10px";
+
+text.style.fontSize = "18px";
+text.style.fontStyle = "italic";
+text.style.color = "darkgreen";
